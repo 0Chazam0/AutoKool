8fd22f65baad72684fdc45a11c1ee1e6fc7f1ccf
PJ <pierrej.goo@gmail.com>
2018-04-06 13:34:30 +0200

    Mission3 init

--- app/src/main/java/com/example/jbeuquila/autokool/MainActivity.java
+++ app/src/main/java/com/example/jbeuquila/autokool/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.jbeuquila.autokool;
 
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonAnnuler = (Button) findViewById(R.id.btnAnnuler);
+        buttonAnnuler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView txtUser;
+                txtUser = (TextView) findViewById(R.id.utilisateur);
+                TextView txtMdp;
+                txtMdp = (TextView) findViewById(R.id.mdp);
+
+                txtUser.setText("");
+                txtMdp.setText("");
+
+            }
+        });
+
+        final Button btnValider = (Button) findViewById(R.id.btnValider);
+        btnValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Selection.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
