5c77d6e369e6e5d2fd32a31cd6dc3077dac6d5ba
0Chazam0 <32390624+0Chazam0@users.noreply.github.com>
2018-04-06 13:15:16 +0200

    initialisation

---
+++ app/src/main/java/com/example/jbeuquila/autokool/Selection.java
@@ -0,0 +1,27 @@
+package com.example.jbeuquila.autokool;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
+
+        final Button btnJerem = (Button) findViewById(R.id.btnJerem);
+        btnJerem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Selection.this, Mission3.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
